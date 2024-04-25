ca0b5b95a6639107529c218dd0501adfa3161ba8
charithvithanage <charithvin@gmail.com>
2024-04-25 23:19:11 +0530

    Initial Commit
    Removed Fat fragments
    Renamed Main Activity

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2021 YUMEMI Inc. All rights reserved.
+ */
+package jp.co.yumemi.android.code_check.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
